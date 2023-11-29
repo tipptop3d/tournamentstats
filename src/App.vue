d1f25e66d54dc7e719d87c062aad6919aa9d3951
tipptop3d <spamtop3d@gmail.com>
2023-11-30 00:02:27 +0100

    BaseButton styled

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-	<RouterView />
+	<div class="wrap">
+		<BaseButton>BaseButton</BaseButton>
+		<ButtonCTA>ButtonCTA</ButtonCTA>
+		<ButtonDelete>ButtonDelete</ButtonDelete>
+		<ButtonDiscordLogin>ButtonDiscordLogin</ButtonDiscordLogin>
+		<ButtonFilledIcon>ButtonFilledIcon</ButtonFilledIcon>
+		<ButtonIconRound>ButtonIconRound</ButtonIconRound>
+		<RouterView />
+	</div>
 </template>
 
 <script setup lang="ts">
+import BaseButton from './components/input/buttons/BaseButton.vue'
+import ButtonCTA from './components/input/buttons/ButtonCTA.vue'
+import ButtonDelete from './components/input/buttons/ButtonDelete.vue'
+import ButtonDiscordLogin from './components/input/buttons/ButtonDiscordLogin.vue'
+import ButtonFilledIcon from './components/input/buttons/ButtonFilledIcon.vue'
+import ButtonIconRound from './components/input/buttons/ButtonIconRound.vue'
+
 import { inject, onMounted, type Ref } from 'vue'
 import { RouterView } from 'vue-router'
 
@@ -23,3 +38,9 @@ onMounted(() => {
 	})
 })
 </script>
+
+<style scoped>
+.wrap {
+	margin: 16px;
+}
+</style>
--- src/components/input/buttons/BaseButton.vue
+++ src/components/input/buttons/BaseButton.vue
@@ -1,7 +1,39 @@
 <template>
-	<div></div>
+	<button class="button">
+		<slot></slot>
+	</button>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.button {
+	padding: 0.8em 1.3em;
+	background-color: var(--main-color);
+	border: none;
+	outline: none;
+	border-radius: 4px;
+	cursor: pointer;
+	transition:
+		background-color 0.2s,
+		box-shadow 0.2s;
+}
+
+.button:hover,
+.button:focus-visible {
+	background-color: color-mix(in srgb, var(--main-color), black 20%);
+}
+.button:active {
+	background-color: color-mix(in srgb, var(--main-color), black 40%);
+}
+
+.button:focus-visible {
+	outline: var(--text-color) 2px solid;
+}
+
+.button:disabled {
+	background-color: color-mix(in srgb, var(--main-color), rgb(87, 87, 87) 90%);
+	opacity: 0.5;
+	cursor: default;
+}
+</style>
